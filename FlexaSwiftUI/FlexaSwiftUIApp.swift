45db433c166a274889f41f1a85004baf5901e5a9
Smithy1232 <holypigs145@gmail.com>
2025-08-30 21:06:43 -0700

    Initial Commit

---
+++ FlexaSwiftUI/FlexaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexaSwiftUIApp.swift
+//  FlexaSwiftUI
+//
+//  Created by Aadjot Singh Sidhu on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FlexaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlexaSwiftUITests/FlexaSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FlexaSwiftUITests.swift
+//  FlexaSwiftUITests
+//
+//  Created by Aadjot Singh Sidhu on 8/30/25.
+//
+
+import Testing
+@testable import FlexaSwiftUI
+
+struct FlexaSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
